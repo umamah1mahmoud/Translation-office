c3a00036e20d87b7ef33372fea3385c4b1f19c51
umamah1mahmoud <umamah.m.94@gmail.com>
2021-02-23 15:41:26 +0200

    edits

--- class-06/JS/Script.js
+++ class-06/JS/Script.js
@@ -4,21 +4,21 @@
 
 
 
-var UserAge=prompt("How old are you?")
+var UserAge=prompt("How old are you?");
 if (UserAge < 18) {
-   console.log("Sorry you're under the legal age! We cannot help you")
+   console.log("Sorry you're under the legal age! We cannot help you");
 }
 
 if (UserAge>=18) {
-    console.log ("Welcome")
+    console.log ("Welcome");
 }
 
 var userT= "translator"
 var userC= "client"
-var UserType=prompt("Why are you here? Are you a tranlator or a client?")
+var UserType=prompt("Why are you here? Are you a tranlator or a client?");
 if (UserType===userT){
-    console.log ("We will gladly contribute in promosing projects!")
+    console.log ("We will gladly contribute in promosing projects!");
 }
 if (UserType===userC){
-    console.log ("Our team will try to help you as much as we can. Please contact us.")
+    console.log ("Our team will try to help you as much as we can. Please contact us.");
 }
