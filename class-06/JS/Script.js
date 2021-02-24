6c4a7125393da557203b92a24910f2ae9b05191b
umamah1mahmoud <umamah.m.94@gmail.com>
2021-02-24 20:26:43 +0200

    change in pic loop

--- class-06/JS/Script.js
+++ class-06/JS/Script.js
@@ -51,8 +51,9 @@ if (wordsNumber > 5000){
 }
 
 // the user rating of this interaction
-var counter=0
+
 var rating= prompt ("Please rate out interactive page from 0 to 5..");
 for (i=0; i<=rating; i++){ 
     document.write('<img src="https://st2.depositphotos.com/1000868/5525/i/950/depositphotos_55257245-stock-photo-3d-yellow-star-isolated-on.jpg" />');
 }
+
